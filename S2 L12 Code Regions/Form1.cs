a9b25a80fc7e789d4771cef34cd7d083835b7e32
joachimharms <joachimharms77@gmail.com>
2017-11-12 10:44:06 +0100

    Implement region with logic - done

---
+++ S2 L12 Code Regions/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace S2_L12_Code_Regions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region SayHelloLogic
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SayHello();
+        }
+
+        private void SayHello()
+        {
+            MessageBox.Show("Hello...");
+        }
+
+        #endregion
+
+    }
+}
