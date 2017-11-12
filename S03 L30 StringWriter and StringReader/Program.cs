21cd3869740cd01a45a2f582db2853ed21cac949
joachimharms <joachimharms77@gmail.com>
2017-11-12 14:53:14 +0100

    Add project StringWriter -Reader to project - not finished

---
+++ S03 L30 StringWriter and StringReader/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S03_L30_StringWriter_and_StringReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ S03 L30 StringWriter and StringReader/StringReadWrite.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S03_L30_StringWriter_and_StringReader
+{
+    public class StringReadWrite
+    {
+        private var sb = new StringBuilder();
+
+        public StringReadWrite()
+        {
+            WriteData();
+            ReadData();
+        }
+
+        private void WriteData()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ReadData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
