a8919cf912a62af15ec40c6dcd67414adf913a69
joachimharms <joachimharms77@gmail.com>
2017-11-12 10:56:58 +0100

    Add Classes and Objects Example project - done

---
+++ S03 L20 Classes and Objects Example/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace S03_L20_Classes_and_Objects_Example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var e1 = new Employee();
+            e1.FirstName = "Mark";
+            e1.LastName = "Long";
+            e1.Seniority = 5;
+
+            var p = e1.Promote();
+            MessageBox.Show(p);
+
+            var e2 = new Employee();
+            e2.FirstName = "Alice";
+            e2.LastName = "Jones";
+        }
+    }
+
+    public class Employee
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Seniority { get; set; }
+
+        public string AnnualReview()
+        {
+            return "Annual review completed...";
+        }
+
+        public string Promote()
+        {
+            return "Employee has been promoted...";
+        }
+    }
+}
---
+++ S03 L20 Classes and Objects Example/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace S03_L20_Classes_and_Objects_Example
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
