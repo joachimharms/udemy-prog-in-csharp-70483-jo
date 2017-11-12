1636ed3fb247551e700c6e33a3172e6aabbc89cc
joachimharms <joachimharms77@gmail.com>
2017-11-12 11:11:27 +0100

    Add exception handling project - done

---
+++ S03 L27 Exceptions Handling/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S03_L27_Exceptions_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 0;
+
+            //Console.WriteLine("a divided by b is: " + a / b);
+
+            ////All code in the try block will be checked for exceptions
+            //try
+            //{
+            //    Console.WriteLine("a divided by b is: " + a / b);
+            //}
+            ////If an exception occurs, the code in the catch block will be run
+            //catch
+            //{
+            //    Console.WriteLine("An error occurred...");
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Finally code has been executed...");
+            //}
+
+            try
+            {
+                Console.WriteLine("a divided by b is: " + a / b);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("The following error has occured : " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("The Finally section has bee run...");
+            }
+
+
+            Console.WriteLine("...the program continous...");
+        }
+    }
+}
