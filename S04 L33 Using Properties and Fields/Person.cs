025439808674d3ea11efa667fe5dedb90ea0891c
joachimharms <joachimharms77@gmail.com>
2017-11-15 23:45:16 +0100

    Finish properties and fields - done

---
+++ S04 L33 Using Properties and Fields/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Dynamic;
+
+namespace S04_L33_Using_Properties_and_Fields
+{
+    public class Person
+    {
+        //// simple field without properties - too easy to access it from the outside and fatfinger it (ie. set age to 120 instead of 12)
+        //public int Age;
+
+        //// instead we try a private field:
+        private int age; // private members are allways lowercase!
+        // Then we create some accessors to handle that field:
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                // We check that value that is passed to set age is a value we like:
+                if ((value > 0) && (value < 65))
+                {
+                    this.age = value;
+                }
+                else
+                {
+                    throw new Exception("Age cannot be negative and not greater than 65.");
+                }
+            }
+        }
+
+        // automatic property with a hidden field:
+        public int Weight { get; set; }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        
+
+
+
+    }
+}
---
+++ S04 L33 Using Properties and Fields/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S04_L33_Using_Properties_and_Fields
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person();
+            p.Age = 92;
+            p.Weight = 35;
+            p.Name = "Piona";
+            Console.WriteLine("The person is " + p.Age + " years old.");
+        }
+    }
+}
