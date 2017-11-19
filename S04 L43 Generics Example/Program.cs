ff4eed4ac0db257b299db8c498b36261266753bf
joachimharms <joachimharms77@gmail.com>
2017-11-19 12:48:31 +0100

    Add Generics part 1 - done

---
+++ S04 L43 Generics Example/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S04_L43_Generics_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Showing Problems with non generics types:
+            System.Collections.ArrayList list1 = new System.Collections.ArrayList();
+            list1.Add(3);
+            list1.Add(5);
+            list1.Add(12);
+            // Der Compiler erlaubt uns auch einen String hinzuzufügen:
+            list1.Add("Hello World");
+
+            // Das Problem ist der String wird implicit geboxed, aber kann nicht
+            // implizit unboxed werden bei der ausgabe. Das Problem ist, dass es
+            // uns erlaubt wurde den falschen Datentypen überhaupt zur Liste hinzuzufügen.
+            // Schmeißt eine InvalidCastException:
+
+            Console.WriteLine("Non-generic list results...");
+            foreach (int x in list1)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
