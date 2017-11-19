5a36f176e71ff40632ba1e960f1211c4ca14640a
joachimharms <joachimharms77@gmail.com>
2017-11-19 19:13:57 +0100

    Casting and Convert class - done

--- S04 L43 Generics Example/Program.cs
+++ S04 L43 Generics Example/Program.cs
@@ -18,7 +18,7 @@ namespace S04_L43_Generics_Example
             list1.Add(5);
             list1.Add(12);
             // Der Compiler erlaubt uns auch einen String hinzuzufügen:
-            list1.Add("Hello World");
+            //list1.Add("Hello World");
 
             // Das Problem ist der String wird implicit geboxed, aber kann nicht
             // implizit unboxed werden bei der ausgabe. Das Problem ist, dass es
@@ -29,6 +29,21 @@ namespace S04_L43_Generics_Example
             foreach (int x in list1)
             {
                 Console.WriteLine(x);
+            }
+
+            // Bessere Implementation mit generics:
+            var list2 = new List<int>();
+            list2.Add(34);
+            list2.Add(7);
+            list2.Add(21);
+
+            // Wir versuchen wieder einen string zur Liste hinzuzufügen:
+            //list2.Add("Hello World");
+
+            Console.WriteLine("\nGeneric list results...");
+            foreach (int x in list2)
+            {
+                Console.WriteLine(x); 
             }
         }
     }
---
+++ S04 L46 CastAndConvertExample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S04_L46_CastAndConvertExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Passt gerade noch in einen integer:
+            int num = 2147483647;
+
+            // Implicit Conversion:
+            long bigNum = num;
+
+            int i = 5;
+            double d = 10.5;
+            // Kann nicht implicit converted werden, daher explicit casting inklusive Datenverlust:
+            i = (int)d;
+            Console.WriteLine("The double variable d converted to int: " + i);
+
+            // explizit casting
+            int x = 7;
+            double y = 12.6;
+            x = (int)y;
+
+            Console.WriteLine("Double casted to int: " + x);
+
+
+            // explizit Converting mittels Convert Klasse
+            int xx = 7;
+            double yy = 12.6;
+            xx = Convert.ToInt32(yy);
+
+            Console.WriteLine("Double casted to int: " + xx);
+
+            // Convert Klasse kann sogar strings to int konvertieren:
+            string str = "123";
+            int r;
+
+            r = Convert.ToInt32(str);
+
+            Console.WriteLine("Convert string to int: " + r);
+        }
+    }
+}
